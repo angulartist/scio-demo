43a9747afe2a1a0c3efb223f158deb38dd9ffbf0
techlead <belabesmohammed@gmail.com>
2019-10-09 10:55:40 +0200

    test wordcount

--- src/test/scala/demo/MainTest.scala
+++ src/test/scala/demo/MainTest.scala
@@ -2,113 +2,78 @@ package demo
 
 import com.spotify.scio.testing._
 import com.spotify.scio.values.WindowOptions
-import demo.WindowParams.groupedWithinTrigger
-import org.apache.beam.sdk.transforms.windowing.{
-  IntervalWindow,
-  TimestampCombiner
-}
+import org.apache.beam.sdk.transforms.windowing.{AfterWatermark, IntervalWindow}
 import org.apache.beam.sdk.values.TimestampedValue
 import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode
 import org.joda.time.{Duration, Instant}
 
-import scala.util.Random
-
 class MainTest extends PipelineSpec {
-  private val random = new Random()
-  private case class Topic(urlkey: String, topic_name: String)
+  private val WINDOW_DURATION: Duration = Duration.standardMinutes(2)
+  private val BASE_TIME: Instant = new Instant(0)
 
-  private case class Group(group_city: String,
-                           group_country: String,
-                           group_topics: List[Topic])
+  private def sampleEvent(
+    record: String,
+    baseTimeOffset: Duration
+  ): TimestampedValue[String] = {
 
-  private case class RSVPEvent(eventId: Int, timestamp: Long, group: Group)
-
-  private val baseTime: Instant = new Instant(0)
-
-  private val firstGroup = Group(
-    "Paris",
-    "France",
-    List(Topic("cpp", "C++ Fans"), Topic("c-sharp", "C# Fans"))
-  )
-
-  private val secondGroup = Group(
-    "London",
-    "UK",
-    List(
-      Topic("php-haters", "Folks who hate PHP a lot!"),
-      Topic("tee-sipping", "We're sipping your tears bruh")
-    )
-  )
-
-  private val thirdGroup = Group(
-    "Madrid",
-    "Spain",
-    List(
-      Topic("c-sharp", "C# Fans"),
-      Topic("angulartist", "Just Angulartists"),
-      Topic("php-haters", "Folks who hate PHP a lot!"),
-      Topic("tee-sipping", "We're sipping your tears bruh")
-    )
-  )
-
-  private def event(eventId: Int,
-                    group: Group,
-                    baseTimeOffset: Duration): TimestampedValue[RSVPEvent] = {
-
-    val timestamp = baseTime.plus(baseTimeOffset)
-
-    TimestampedValue.of(
-      RSVPEvent(eventId, timestamp.getMillis, group),
-      timestamp
-    )
+    TimestampedValue.of(record, BASE_TIME.plus(baseTimeOffset))
   }
 
-  "LeaderBoard.calculateTeamScores" should "work with on time elements" in {
-    val stream = testStreamOf[RSVPEvent]
-      .advanceWatermarkTo(baseTime)
+  "Main" should "work with on time elements" in {
+    val stream = testStreamOf[String]
+      .advanceWatermarkTo(BASE_TIME)
       .addElements(
-        event(1, firstGroup, Duration.standardSeconds(10)),
-        event(2, secondGroup, Duration.standardSeconds(40))
+        // Sample Event
+        sampleEvent(record = "Cat Cat Dog", Duration.standardSeconds(10)),
+        // Sample Event
+        sampleEvent(record = "Dog Bird Cat", Duration.standardSeconds(20)),
+        // Empty Line
+        sampleEvent(record = "", Duration.standardSeconds(30))
       )
-      //    .advanceWatermarkTo(baseTime.plus(Duration.standardSeconds(60)))
-      //    .addElements(event(3, thirdGroup, Duration.standardSeconds(80)))
+      // Advance Watermark to the end of the window
+      .advanceWatermarkTo(BASE_TIME.plus(Duration.standardMinutes(3)))
+      .addElements(
+        // After end of the window
+        sampleEvent(record = "Dog", Duration.standardMinutes(4))
+      )
       .advanceWatermarkToInfinity
-
-    val windowDuration = Duration.standardMinutes(5)
-
-    val currentWindow = new IntervalWindow(baseTime, windowDuration)
 
     runWithContext { sc =>
       val pipeline = sc
         .testStream(stream)
-        .transform("Filter and Flatten Topics") {
-          _.filter { x: RSVPEvent =>
-            x.group.group_topics.nonEmpty
-          }.flatMap { x: RSVPEvent =>
-            x.group.group_topics
+        .transform("Filter Words") {
+          _.map { x: String =>
+            x.trim
+          }.filter { x: String =>
+            x.nonEmpty
           }
+        }
+        .transform("Tokenize Words") {
+          _.flatMap(
+            _.split("[^a-zA-Z']+")
+              .filter(_.nonEmpty)
+          )
         }
         .transform("Assign a fixed-time window") {
           _.withFixedWindows(
-            duration = Duration.standardMinutes(5),
+            duration = WINDOW_DURATION,
             options = WindowOptions(
-              trigger = groupedWithinTrigger,
+              trigger = AfterWatermark.pastEndOfWindow(),
               accumulationMode = AccumulationMode.DISCARDING_FIRED_PANES,
-              allowedLateness = Duration.ZERO,
-              timestampCombiner = TimestampCombiner.END_OF_WINDOW
+              allowedLateness = Duration.standardMinutes(2)
             )
           )
         }
-        .transform("Sum topics per key") {
-          _.map[(String, Int)] { x: Topic =>
-            (s"${x.urlkey}", 1)
+        .transform("Pair Word With One") {
+          _.map[(String, Int)] { x: String =>
+            (x, 1)
           }.sumByKey
         }
 
-      pipeline should inOnTimePane(currentWindow) {
-        containInAnyOrder(
-          Seq(("cpp", 1), ("c-sharp", 1), ("php-haters", 1), ("tea-sipping", 1))
-        )
+      pipeline should inFinalPane(
+        new IntervalWindow(BASE_TIME, WINDOW_DURATION)
+      ) {
+        containInAnyOrder(Seq(("Cat", 3), ("Dog", 2), ("Bird", 1)))
       }
     }
   }
